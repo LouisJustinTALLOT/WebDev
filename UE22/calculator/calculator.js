da14cedda473ab1ccb25b21ba7dfb5e82ff3ce1f
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-02-28 13:33:53 +0100

    on reset tout avec le bouton C

--- UE22/calculator/calculator.js
+++ UE22/calculator/calculator.js
@@ -8,3 +8,9 @@ function update_display(string_to_display) {
     let div_display = document.getElementById("display");
     div_display.innerHTML = string_to_display;
 }
+
+function reset(){
+    expression = "";
+    update_display("");
+}
+
