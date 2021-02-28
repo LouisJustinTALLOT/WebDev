29f96229e557a242bcf669950baf0ffa03c42ad9
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-02-28 13:37:54 +0100

    typo fix

--- UE22/calculator/calculator.js
+++ UE22/calculator/calculator.js
@@ -2,7 +2,7 @@
 
 let expression = "";
 let is_displayed_main = "";
-let is_displayed_secondary
+let is_displayed_secondary = "";
 
 function update_display(string_to_display) {
     let div_display = document.getElementById("display");
