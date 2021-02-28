1b6c5793c3e3f53406bdf91b3c47037df4b21bc1
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-02-28 13:12:44 +0100

    function to change what is displayed

--- UE22/calculator/calculator.js
+++ UE22/calculator/calculator.js
@@ -1,1 +1,6 @@
 // script for the calculator
+
+function update_display(string_to_display) {
+    let div_display = document.getElementById("display");
+    div_display.innerHTML = string_to_display;
+}
