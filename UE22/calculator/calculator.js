52a1dd3aee9fd2f9698553d1f2eb8d622b758607
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-02-28 13:33:05 +0100

    chaînes de caractères utiles

--- UE22/calculator/calculator.js
+++ UE22/calculator/calculator.js
@@ -1,5 +1,9 @@
 // script for the calculator
 
+let expression = "";
+let is_displayed_main = "";
+let is_displayed_secondary
+
 function update_display(string_to_display) {
     let div_display = document.getElementById("display");
     div_display.innerHTML = string_to_display;
